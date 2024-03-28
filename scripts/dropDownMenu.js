52c317e6e80fe7ebaff13274dfff655d334950ac
liminfinity <108136720+liminfinity@users.noreply.github.com>
2024-03-28 14:41:16 +0700

    transfered svg to separating files

--- scripts/dropDownMenu.js
+++ scripts/dropDownMenu.js
@@ -1,7 +1,7 @@
 const dropdown = document.querySelector('.select');
 const dropdownResult = document.querySelector('.select__current-value');
 const dropdownMenu = document.querySelector('.select__list');
-
+let ariaExpanded = false;
 
 document.addEventListener('click', e => {
     if (!e.target.closest('.select')) dropdownMenu.classList.remove('visibile')
@@ -15,13 +15,16 @@ dropdownMenu.querySelectorAll('.select__option').forEach(option => {
     option.addEventListener('click', _ => {
         const selectedOption = document.querySelector('.select__option_selected');
         selectedOption.classList.remove('select__option_selected');
+        selectedOption.setAttribute('aria-selected', 'false')
         option.classList.add('select__option_selected')
         dropdownResult.innerHTML = option.innerHTML
+        option.setAttribute('aria-selected', 'true')
         toogleDropDown()
     })
 })
 
 function toogleDropDown() {
+    dropdown.setAttribute('aria-expanded', ariaExpanded = !ariaExpanded)
     dropdown.classList.toggle('select_open')
     dropdownMenu.classList.toggle('visibile')
 }
